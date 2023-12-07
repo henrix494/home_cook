72a5f61a0b0d7d4a309e2ae73bfc02ff846c3b15
henrix494 <henrix494@gmail.com>
2023-12-07 20:12:02 +0200

    addes style

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  matcher: [
+    "/((?!api|_next/static|_next/image|assets|favicon.ico|logo.png|sw.js).*)",
+  ],
 };
 
 module.exports = nextConfig;
