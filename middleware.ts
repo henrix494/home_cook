521a340c4099344966446566198a1ac4c11b65a0
henrix494 <henrix494@gmail.com>
2023-12-07 21:13:45 +0200

    fixed IMG

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
 import { withAuth } from "next-auth/middleware";
-
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
+    "/partner/:path*",
+  ],
+};
 export default withAuth({
   pages: {
     signIn: "/signIn",
   },
 });
-export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|assets|favicon.ico|logo.png|sw.js).*)",
-  ],
-};
