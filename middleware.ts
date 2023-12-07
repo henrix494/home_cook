cb9bc39d92c9f5168e6543be5ba9a1fa0b7df68d
henrix494 <henrix494@gmail.com>
2023-12-07 21:19:00 +0200

    fixed IMG

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,7 @@
 import { withAuth } from "next-auth/middleware";
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|auth/login).*)",
-    "/partner/:path*",
+    "/((?!api|_next/static|_next/image|public|paula.png|natan.png|sw.js).*)",
   ],
 };
 export default withAuth({
