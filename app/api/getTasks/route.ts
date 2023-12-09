af35a88cddcf5459e7bb906f24788a2fc756529f
henrix494 <henrix494@gmail.com>
2023-12-09 23:07:43 +0200

    added main func

--- app/api/getTasks/route.ts
+++ app/api/getTasks/route.ts
@@ -1,9 +1,10 @@
 import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
 
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest, respone: NextResponse) {
   const { rows } = await sql`SELECT * FROM tasks `;
-
+  revalidatePath("/");
   return NextResponse.json({ data: rows });
 }
