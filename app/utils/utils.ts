df34cffa4950f811bb48655ff7cd26a309e02114
henrix494 <henrix494@gmail.com>
2023-12-10 09:38:55 +0200

    fixed delay

--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres";
 export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 export const getItem = async () => {
   const { rows } = await sql`SELECT * FROM tasks `;
