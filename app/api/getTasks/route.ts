886dddaf455dfd3cb59f315ef98a012d41e6f751
henrix494 <henrix494@gmail.com>
2023-12-10 10:29:15 +0200

    fixed delay

--- app/api/getTasks/route.ts
+++ app/api/getTasks/route.ts
@@ -1,6 +1,8 @@
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
-
+export const fetchCache = "force-no-store";
+export const revalidate = 0;
+export const dynamic = "force-dynamic";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest, respone: NextResponse) {
